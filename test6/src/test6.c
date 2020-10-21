e2ceb238b3166534b9f8edfc7baa0772aa1f5cf5
root <root@jarek-virtual-machine>
2020-10-21 08:39:30 +0200

    pierwsze przeslanie

---
+++ test6/src/test6.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : test6.c
+ Author      : Krygier
+ Version     :
+ Copyright   : ISL_WEL WAT
+ Description : test6
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("test6"); /* prints test6 */
+	return EXIT_SUCCESS;
+}
